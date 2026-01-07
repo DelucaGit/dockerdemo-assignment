b7f28a24c56116fed2d72766a998567f550afb0f
DelucaGit <andalusi.private@gmail.com>
2026-01-07 20:24:14 +0200

    Initial commit med Dockerfile och Workflow

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+# du kan ändra till din version av java om du använder nyare
+FROM eclipse-temurin:17-jre
+WORKDIR /app
+# ändra target/app.jar till target/<namnet_på_din_jar_fil>
+COPY target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/se/jensen/marcel/dockerdemo/controllers/DockerController.java
@@ -0,0 +1,15 @@
+package se.jensen.marcel.dockerdemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+
+    @GetMapping("/welcome")
+    public String welcomeToDocker(){
+        return "Welcome to docker! - Built by Github Actions";
+    }
+}
